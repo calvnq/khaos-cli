36cb16f70d15b363649fbd172d95691ce7bf4c3d
Cameron J Roe <cameronjroe@gmail.com>
2016-04-05 01:25:27 -0400

    initial

---
+++ readme.md
@@ -0,0 +1,19 @@
+# khaos-cli
+
+A Khaos template to start new cli projects.
+
+## Installation
+
+Save the template locally with:
+
+    $ khaos install calvnq/khaos-cli cli
+
+## Usage
+
+Create a new node.js project with:
+
+    $ khaos create cli my-project
+
+## License
+
+[MIT](/LICENSE)
---
+++ template/test/prompts/setup.test.js
@@ -0,0 +1,28 @@
+import { setupPrompt } from 'prompts/setup';
+
+describe('(Prompts) #setupPrompt', () => {
+  let start, prompt;
+
+  beforeEach(() => {
+    start = sinon.spy();
+    prompt = { start: start };
+    setupPrompt('testing', prompt);
+  });
+
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+  
+  it('gives the prompt a custom message', () => {
+    expect(prompt.message).to.match(/testing/);
+  });
+
+  it('sets the delimiter to be an empty string', () => {
+    expect(prompt.delimiter).to.eql('');
+  });
+
+  it('starts the prompt', () => {
+    expect(start.calledOnce).to.be.true;
+  });
+
+});
